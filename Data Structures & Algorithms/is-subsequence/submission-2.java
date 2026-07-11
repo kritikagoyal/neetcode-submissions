c25f5a9efae10887b1eeac35ccb6d336effa7eb8
Kritika Goyal <123626753+kritikagoyal@users.noreply.github.com>
2026-07-11 18:08:39 +0530

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0; // pointer for s
+        for (int j = 0; j < t.length(); j++) {
+            if (i == s.length()) break;
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+        }
+        return i == s.length();
+    }
+}
