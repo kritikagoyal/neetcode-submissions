2b4156842b907cb17a10eeb2984ab0bb2cb6bf95
Kritika Goyal <123626753+kritikagoyal@users.noreply.github.com>
2026-08-13 11:46:11 +0530

    Add: next-greater-element-i - submission-0

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums2.length; i++) {
+            while (!stack.isEmpty() && stack.peek() < nums2[i]) {
+                int poppedElement = stack.pop();
+                map.put(poppedElement, nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+        
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.getOrDefault(nums1[i], -1);
+        }
+        
+        return result;
+    }
+}
